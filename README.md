00ff28058848271f08d02feb24e35e44a681666a
projektir <oprojektir@gmail.com>
2019-07-10 17:05:03 +0200

    Add basic text input convert

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+Pipeline so parallel, very async, wow.
+
 Can run pipeline with:
 
 `cargo run --bin divvun-pipeline -- myfile.zpipe`
@@ -7,3 +9,17 @@ Can run pipeline with:
 To generate a 0 compression pipeline zip file (on Unix):
 
 `zip -0 pipeline.zpipe pipeline.json`
+
+To run tests:
+
+On Mac:
+
+`./test.sh`
+
+On Linux:
+
+`./test-linux.sh`
+
+On Windows:
+
+Exercise for the reader, but probably modify one of the other files to refer to `.dll` files.
---
+++ divvun-pipeline/src/bin/zinput-convert.rs
@@ -0,0 +1,29 @@
+use std::io::{self};
+
+use clap::{crate_version, App, Arg};
+
+use divvun_schema::{capnp_message, string_capnp::string};
+
+fn main() {
+    let matches = App::new("zinput-convert")
+        .version(crate_version!())
+        .author("projektir <oprojektir@gmail.com>")
+        .about("Utility for converting input into a divvun-pipeline compatible format.")
+            .arg(Arg::with_name("text")
+                .value_name("PATH")
+                .help("Convert text")
+                .short("t")
+                .long("text")
+                .takes_value(true)
+                .required(true)
+            )
+        .get_matches();
+
+    if let Some(text) = matches.value_of("text") {
+        let msg = capnp_message!(string::Builder, builder => {
+            builder.set_string(text);
+        });
+
+        capnp::serialize::write_message(&mut io::stdout(), &msg).unwrap();
+    }
+}
